3eb5b561991a71deaf447dbf1a47a242a2655476
YUSUKE SHIBUYA <shibusan@staygold.jp>
2022-06-15 06:28:49 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/yusuke_shibuya-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "yusuke_shibuya"
+  bucket_prefix = "yusuke-shibuya"
 }
